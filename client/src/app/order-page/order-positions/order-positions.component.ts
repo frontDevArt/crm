a2a7974e9fbe58332a4ab8c00e9797adf3f57f95
arturgevorgyan <frontdevart@gmai.com>
2020-04-23 16:33:41 +0400

    orders compleated

---
+++ client/src/app/order-page/order-positions/order-positions.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {PositionsService} from "../../shared/services/positions.service";
+import {Observable} from "rxjs";
+import {Position} from "../../shared/interfaces";
+import {map, switchMap} from "rxjs/operators";
+import {OrderService} from "../order.service";
+import {MaterialService} from "../../shared/classes/material.service";
+
+@Component({
+  selector: 'app-order-positions',
+  templateUrl: './order-positions.component.html',
+  styleUrls: ['./order-positions.component.css']
+})
+export class OrderPositionsComponent implements OnInit {
+
+  position$: Observable<Position[]>
+
+  constructor(private route: ActivatedRoute,
+              private positionsService: PositionsService,
+              private order: OrderService) { }
+
+  ngOnInit(): void {
+    this.position$ = this.route.params
+      .pipe(
+        switchMap(
+          (params: Params) => {
+            return this.positionsService.fetch(params['id'])
+          }
+        ),
+          map(
+            (positions: Position[]) => {
+              return positions.map(position => {
+                position.quantity = 1
+                return position
+              })
+            }
+          )
+      )
+  }
+
+  addOrder(position: Position) {
+    MaterialService.toast(`Добалена Х${position.quantity}`)
+    this.order.add(position)
+  }
+}
